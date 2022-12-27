ddc7b9b5ff131dc43853a3adde297fc0ceb50c42
Long <119793901+tran-hai-long@users.noreply.github.com>
2022-12-28 00:32:59 +0700

    Enforce password validation when registering a new user

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+
+from django.contrib.auth.password_validation import validate_password
 
 from registration.forms import RegistrationForm
 
@@ -10,6 +13,11 @@ def registration_page(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            try:
+                validate_password(form.cleaned_data["password"])
+            except ValidationError as e:
+                context = {"form": RegistrationForm(), "password_error": str(e)}
+                return render(request, "registration/registration.html", context)
             User.objects.create_user(
                 form.cleaned_data["username"],
                 form.cleaned_data["email"],
