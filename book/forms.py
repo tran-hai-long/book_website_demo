82c1295febebf45e6668fb611fc1726217479d3d
Long <119793901+tran-hai-long@users.noreply.github.com>
2022-12-28 12:50:20 +0700

    Improve Review HTML form aesthetic

--- book/forms.py
+++ book/forms.py
@@ -1,4 +1,4 @@
-from django.forms import Form, CharField, TextInput, ModelForm
+from django.forms import Form, CharField, TextInput, ModelForm, NumberInput, Textarea
 
 from book.models import Review
 
@@ -11,3 +11,12 @@ class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = ("rating", "comment")
+        widgets = {
+            "rating": NumberInput(attrs={"class": "form-control mb-3"}),
+            "comment": Textarea(attrs={"class": "form-control mb-3"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        self.fields["rating"].widget.attrs["min"] = 1
+        self.fields["rating"].widget.attrs["max"] = 5
